e013cb4b8ab34acc21ab0677e7a7ed54dd5c9271
icteambli-spec <icteambli@gmail.com>
2026-04-08 21:00:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ cloudinary.config(
 PUBLIC_FILE_ID = "data_saybread.xlsx"
 PUBLIC_PERIODE_ID = "periode_saybread.json"
 
-st.title("🍞 Portal Say Bread")
+st.title("🍞 Monitoring Say Bread")
 
 # ==========================================
 # 3. FUNGSI BANTU
@@ -219,7 +219,7 @@ with tab_dsi:
     st.markdown(f"#### 📅 Periode Data: `{periode_dict.get('DSI_FD', 'Belum diatur')}`")
     st.write("")
 
-    input_toko_dsi = st.text_input("🔍 Masukkan 4 Digit Kode Toko (Kosongkan untuk melihat Top 10):", max_chars=4, placeholder="Contoh: F08C", key="input_dsi").upper()
+    input_toko_dsi = st.text_input("🔍 Masukkan 4 Digit Kode Toko Untuk Melihat Detail Item Per Toko (Kosongkan untuk melihat Top 10 Toko Rusak Tertinggi):", max_chars=4, placeholder="Contoh: F08C", key="input_dsi").upper()
     btn_enter_dsi = st.button("Enter ↵", key="btn_dsi", type="primary")
 
     with st.spinner("Memuat data..."):
